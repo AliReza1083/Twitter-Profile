8660e844fe6b6190538df852d2c9f62afa03af11
Ali Reza <webdeve1083@gmail.com>
2023-01-28 10:05:50 +0800

    gradient is responsive

--- src/components/Gradients.jsx
+++ src/components/Gradients.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { IoMdArrowDropright } from "react-icons/io";
+import { motion } from "framer-motion";
 
 const GRADIENTS = [
   {
@@ -29,20 +31,49 @@ const GRADIENTS = [
 ];
 
 const Gradients = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const dispatch = useDispatch();
   const rotateSelector = useSelector((state) => state.background.rotate);
+  const backgroundSelector = useSelector(
+    (state) => state.background.background
+  );
   return (
-    <div className="flex lg:flex-col gap-2">
+    <div className="relative flex lg:flex-col gap-2 mt-4">
       {/* <div className="w-24 h-24 bg-[#62BDFF]"></div> */}
-      {GRADIENTS.map((gradient) => (
+      <div className="flex items-center">
         <div
-          className={`w-12 h-12 rounded-md active:scale-90 duration-100`}
+          className="w-12 h-12 rounded-lg"
           style={{
-            background: `linear-gradient(${rotateSelector}deg, ${gradient.from}, ${gradient.to})`,
+            background: `linear-gradient(${rotateSelector}deg, ${backgroundSelector.from}, ${backgroundSelector.to})`,
           }}
-          onClick={() => dispatch({ type: "background", payload: gradient })}
         ></div>
-      ))}
+        <IoMdArrowDropright
+          className={`text-2xl cursor-pointer ${
+            isOpen && "translate-x-3"
+          } duration-150`}
+          onClick={() => setIsOpen(!isOpen)}
+        />
+      </div>
+      {isOpen && (
+        <div
+          id="gradient_container"
+          className="w-full absolute -left-3/4 lg:top-0 lg:left-28 flex lg:flex-col gap-2 overflow-visible"
+        >
+          {GRADIENTS.map((gradient, index) => (
+            <motion.div
+              className={`w-[48px] h-[48px] rounded-md active:!scale-90 duration-100`}
+              style={{
+                background: `linear-gradient(${rotateSelector}deg, ${gradient.from}, ${gradient.to})`,
+              }}
+              animate={{ opacity: [0, 1], x: [-40, 0] }}
+              transition={{ duration: 0.2, delay: index * 0.05 }}
+              onClick={() =>
+                dispatch({ type: "background", payload: gradient })
+              }
+            ></motion.div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { useDispatch } from "react-redux";
 const Navbar = () => {
   const dispatch = useDispatch();
   return (
-    <div className="bg-white fixed bottom-4 lg:top-24 lg:left-8 shadow-xl p-4 rounded-lg flex lg:flex-col items-center gap-4">
+    <div className="w-11/12 lg:w-auto bg-white fixed bottom-24 lg:top-24 lg:left-8 shadow-xl p-4 rounded-lg flex lg:flex-col items-center gap-4 z-50">
       <CircularSlider
         width={80}
         valueFontSize="20px"
