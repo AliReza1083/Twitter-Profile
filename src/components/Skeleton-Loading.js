e0b0ed753f54c82ad85d4d1044187577b1ba7b5b
Ali Reza <webdeve1083@gmail.com>
2023-01-30 14:15:48 +0800

    Fixed a bug: added a key props for each skeleton loaders

--- src/components/Skeleton-Loading.js
+++ src/components/Skeleton-Loading.js
@@ -3,8 +3,11 @@ import React from "react";
 const SkeletonLoading = () => {
   return (
     <div className="grid grid-cols-4 gap-4">
-      {[0, 1, 2, 3, 4, 5, 6].map((skeleton) => (
-        <div className="bg-[#ddd] h-40 rounded-lg animate-blink"></div>
+      {[0, 1, 2, 3, 4, 5, 6].map(([], index) => (
+        <div
+          key={index}
+          className="bg-[#ddd] h-40 rounded-lg animate-blink"
+        ></div>
       ))}
     </div>
   );
