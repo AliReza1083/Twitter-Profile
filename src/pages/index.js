5e019dbb8ec42b0f45541079e14efe97bdc3c07f
Ali Reza <webdeve1083@gmail.com>
2023-01-31 12:00:48 +0800

    Brought some new changes on the responsiveness of the hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,9 +22,10 @@ const Home = () => {
       <header className="relative w-full max-w-[1536px] px-4 md:px-24 lg:px-40 h-screen 2xl:h-auto py-28 2xl:py-56 flex sm:items-center mx-auto overflow-hidden">
         <div>
           <h1 className="uppercase text-3xl font-bold">
-            create an image of your <br /> Twitter Profile
+            create an image of your <br className="hidden md:block" /> Twitter
+            Profile
           </h1>
-          <p className="px-4 py-1 bg-[#ECECEC] rounded-full text-sm mt-2">
+          <p className="px-4 py-1 bg-[#ECECEC] rounded-full text-xs sm:text-sm mt-2">
             Right now there are <span className="text-blue-800">10</span> image
             in the{" "}
             <Link href={"/community-showcase"} className="text-blue-800">
@@ -38,9 +39,9 @@ const Home = () => {
             Create Now
           </Link>
         </div>
-        <div className="absolute right-0 bottom-0 -z-40 w-[550px] lg:w-[700px] xl:w-[900px]">
+        <div className="absolute right-0 bottom-0 -z-40 w-[650px] lg:w-[700px] xl:w-[900px]">
           <Image src={HeaderIMG} alt="" />
-          <div className="w-full h-full bg-gradient-to-l from-transparent to-white absolute top-0 left-0"></div>
+          <div className="w-full h-full bg-gradient-to-tl md:bg-gradient-to-l from-transparent to-white absolute top-0 left-0"></div>
         </div>
       </header>
     </div>
